0d34bbb16aef57ecf38d7691a736091e210d2c64
Davide Cavallini <davidecavallini1987@gmail.com>
2022-07-21 17:04:00 +0200

    aggiunto PARAMETRO_VULNERABILE_XSS

--- jsBugHuntingHelper.js
+++ jsBugHuntingHelper.js
@@ -48,6 +48,71 @@ var searchElements = [
   new SearchElement('REGEX email address', 'regEx', /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   new SearchElement('REGEX API Key', 'regEx', /^[a-f0-9]{32}$/)
   
+  function getAllUrlParams(url) {
+
+    // get query string from url (optional) or window
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+  
+    // we'll store the parameters here
+    var obj = {};
+  
+    // if query string exists
+    if (queryString) {
+  
+      // stuff after # is not part of query string, so get rid of it
+      queryString = queryString.split('#')[0];
+  
+      // split our query string into its component parts
+      var arr = queryString.split('&');
+  
+      for (var i = 0; i < arr.length; i++) {
+        // separate the keys and the values
+        var a = arr[i].split('=');
+  
+        // set parameter name and value (use 'true' if empty)
+        var paramName = a[0];
+        var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+  
+        // (optional) keep case consistent
+        paramName = paramName.toLowerCase();
+        if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+  
+        // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+        if (paramName.match(/\[(\d+)?\]$/)) {
+  
+          // create key if it doesn't exist
+          var key = paramName.replace(/\[(\d+)?\]/, '');
+          if (!obj[key]) obj[key] = [];
+  
+          // if it's an indexed array e.g. colors[2]
+          if (paramName.match(/\[\d+\]$/)) {
+            // get the index value and add the entry at the appropriate position
+            var index = /\[(\d+)\]/.exec(paramName)[1];
+            obj[key][index] = paramValue;
+          } else {
+            // otherwise add the value to the end of the array
+            obj[key].push(paramValue);
+          }
+        } else {
+          // we're dealing with a string
+          if (!obj[paramName]) {
+            // if it doesn't exist, create property
+            obj[paramName] = paramValue;
+          } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+            // if property does exist and it's a string, convert it to an array
+            obj[paramName] = [obj[paramName]];
+            obj[paramName].push(paramValue);
+          } else {
+            // otherwise add the property
+            obj[paramName].push(paramValue);
+          }
+        }
+      }
+    }
+  
+    return obj;
+  }
+  
 
   function getjQueryEventHandlers(element, eventns) {
     const $ = window.jQuery;
@@ -209,6 +274,23 @@ function searchJqueryListeners () {
   recursiveEnumerate(jQueryListeners, 0)
 }
 
+async function testUrlParamFiltering () {
+  const params = getAllUrlParams(document.location.href)
+
+  const mod = Object.entries(params).map((v) => {
+    return v[0] + '=' + v[1] + '"><script>console.log("PARAMETRO_VULNERABILE_XSS")</script><'
+  })
+
+  const newUrl= document.location.origin + document.location.pathname + '?' + mod.join('&')
+
+  await $.get(newUrl).done(function( data ) {
+    if(data.indexOf('PARAMETRO_VULNERABILE_XSS')!==-1){
+      console.log('Parametri Vulnerabili ad attachi XSS')
+      console.log(data)
+    }
+  })
+}
+
 console.log('Created by Davide Cavallini')
 console.log('Linkedin: https://www.linkedin.com/in/davidecavallini/')
 console.log('----------------------------------------------------------')
@@ -219,6 +301,8 @@ recursiveEnumerate(listAllEventListeners(), 0)
 searchInside(document.body.innerHTML.replace(/(\r\n|\n|\r)/gm, '').replace(/\s\s+/g, ' '), document.body, ['BODY'], 0, 0)
 recursiveEnumerate(getjQueryEventHandlers(document), 0)
 searchJqueryListeners()
+console.log('Contenuto cookie', document.cookie)
+testUrlParamFiltering()
 
 console.log('\n')
 console.log('----------------------------------------------------------')
