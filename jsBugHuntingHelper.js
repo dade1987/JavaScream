deda9652dd15fd062a2671a33d0a56728df72502
Davide Cavallini <davidecavallini1987@gmail.com>
2022-07-21 16:46:23 +0200

    jquery listeners added

--- jsBugHuntingHelper.js
+++ jsBugHuntingHelper.js
@@ -22,14 +22,15 @@ var searchElements = [
   new SearchElement('url', 'string', 'https://'),
   new SearchElement('web socket', 'string', 'ws://'),
   new SearchElement('web socket', 'string', 'wss://'),
-  new SearchElement('single line comment', 'string', ' //'),
-  new SearchElement('block comment', 'string', '/*'),
+  //new SearchElement('single line comment', 'string', ' //'),
+  //new SearchElement('block comment', 'string', '/*'),
   new SearchElement('post request', 'string', '"POST"'),
   new SearchElement('get request', 'string', '"GET"'),
   new SearchElement('post request', 'string', "'POST"),
   new SearchElement('get request', 'string', "'GET'"),
-  new SearchElement('ajax request', 'string', '.ajax('),
-  new SearchElement('ajax post request', 'string', '.post('),
+  new SearchElement('ajax request', 'string', '.ajax'),
+  new SearchElement('post request', 'string', '$.post'),
+  new SearchElement('get request', 'string', '$.get'),
   new SearchElement('query', 'string', 'query'),
   new SearchElement('api call', 'string', '/api'),
   new SearchElement('php file', 'string', '.php'),
@@ -38,14 +39,72 @@ var searchElements = [
   new SearchElement('mailto protocol', 'string', 'mailto:'),
   new SearchElement('something on mysql', 'string', 'mysql'),
   new SearchElement('something on email', 'string', 'email'),
-  new SearchElement('something on username', 'string', 'username'),
-  new SearchElement('something on username', 'string', 'user'),
-  new SearchElement('something on password', 'string', 'password'),
-  new SearchElement('something on password', 'string', 'pass'),
-  new SearchElement('something on password', 'string', 'psw'),
-  new SearchElement('something on password', 'string', 'pwd')]
+  new SearchElement('something on username', 'string', '"username"'),
+  new SearchElement('something on username', 'string', '"user"'),
+  new SearchElement('something on password', 'string', '"password"'),
+  new SearchElement('something on password', 'string', '"pass"'),
+  new SearchElement('something on password', 'string', '"psw"'),
+  new SearchElement('something on password', 'string', '"pwd"')]
   new SearchElement('REGEX email address', 'regEx', /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   new SearchElement('REGEX API Key', 'regEx', /^[a-f0-9]{32}$/)
+  
+
+  function getjQueryEventHandlers(element, eventns) {
+    const $ = window.jQuery;
+    const i = (eventns || '').indexOf('.'),
+      event = i > -1 ? eventns.substr(0, i) : eventns,
+      namespace = i > -1 ? eventns.substr(i + 1) : void(0),
+      handlers = Object.create(null);
+    element = $(element);
+    if (!element.length) return handlers;
+    // gets the events associated to a DOM element
+    const listeners = $._data(element.get(0), "events") || handlers;
+    const events = event ? [event] : Object.keys(listeners);
+    if (!eventns) return listeners; // Object with all event types
+    events.forEach((type) => {
+      // gets event-handlers by event-type or namespace
+      (listeners[type] || []).forEach(getHandlers, type);
+    });
+    // eslint-disable-next-line
+    function getHandlers(e) {
+      const type = this.toString();
+      const eNamespace = e.namespace || (e.data && e.data.handler);
+      // gets event-handlers by event-type or namespace
+      if ((event === type && !namespace) ||
+          (eNamespace === namespace && !event) ||
+          (eNamespace === namespace && event === type)) {
+        handlers[type] = handlers[type] || [];
+        handlers[type].push(e);
+      }
+    }
+    return handlers;
+  }
+
+  function listAllEventListeners() {
+    const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
+    allElements.push(document);
+    allElements.push(window);
+  
+    const types = [];
+  
+    for (let ev in window) {
+      if (/^on/.test(ev)) types[types.length] = ev;
+    }
+  
+    let elements = [];
+    for (let i = 0; i < allElements.length; i++) {
+      const currentElement = allElements[i];
+      for (let j = 0; j < types.length; j++) {
+        if (typeof currentElement[types[j]] === 'function') {
+          elements.push(currentElement[types[j]])
+        } 
+      }
+    }
+  
+    return elements/* .sort(function(a,b) {
+      return a.type.localeCompare(b.type);
+    }); */
+  }
 
 function regexIndexOf(string, regex, startpos) {
   var indexOf = string.substring(startpos || 0).search(regex);
@@ -53,7 +112,6 @@ function regexIndexOf(string, regex, startpos) {
 }
 
 function regexAllIndexOf(string, regex, startpos) {
-  const indexes = [];
 
   regex = (regex.global) ? regex : new RegExp(regex.source, "g" + (regex.ignoreCase ? "i" : "") + (regex.multiLine ? "m" : ""));
   if(typeof (startpos) == "undefined") {
@@ -82,10 +140,10 @@ function getAllIndexes (arr, val) {
 
 function searchInside (functionToString, object, objKeys, o, level) {
   if(objKeys[o] === undefined) {
-    objKeys[o] = 'BODY'
+    objKeys[o] = ''
   }
   if(object[objKeys[o]] === undefined) {
-    object[objKeys[o]] = 'BODY'
+    object[objKeys[o]] = ''
   }
   
   searchElements.forEach((v) => {
@@ -107,14 +165,28 @@ function recursiveEnumerate (object, level) {
 
   level++
   const objKeys = Object.keys(object)
+  // console.log("A", object)
+
   for (let o = 0; o < objKeys.length; o++) {
-    if (object[objKeys[o]] !== null && alreadyProcessedFunctions.indexOf(objKeys[o]) === -1 && (typeof object[objKeys[o]] === 'function' || typeof object[objKeys[o]] === 'object') && objKeys[o] !== 'recursiveEnumerate' && objKeys[o] !== 'alreadyProcessedFunctions') {
-      alreadyProcessedFunctions.push(objKeys[o])
+    if (object[objKeys[o]] !== null && (typeof object[objKeys[o]] === 'function' || typeof object[objKeys[o]] === 'object') && objKeys[o] !== 'recursiveEnumerate' && objKeys[o] !== 'alreadyProcessedFunctions') {
+      
+
       try {
         const functionToString = object[objKeys[o]].toString().replace(/(\r\n|\n|\r)/gm, '').replace(/\s\s+/g, ' ')
-        searchInside(functionToString, object, objKeys, o, level)
-        if (objKeys[o] !== 'set' && objKeys[o] !== 'push') {
-          recursiveEnumerate(object[objKeys[o]], level)
+
+        // console.log("B", functionToString)
+
+        if(alreadyProcessedFunctions.indexOf(functionToString) === -1){
+          // console.log("C", functionToString)
+          searchInside(functionToString, object, objKeys, o, level)
+
+          if(functionToString.indexOf('[object Object]')===-1){
+          alreadyProcessedFunctions.push(functionToString)
+        }
+          
+          if (objKeys[o] !== 'set' && objKeys[o] !== 'push') {
+            recursiveEnumerate(object[objKeys[o]], level)
+          }
         }
       } catch (e) {
 
@@ -123,13 +195,30 @@ function recursiveEnumerate (object, level) {
   }
 }
 
+function searchJqueryListeners () {
+  var jQueryListeners = []
+  $( "*" ).each((i,v)=>
+    {
+      const elementListeners = getjQueryEventHandlers(v)
+       //console.log(Object.values(elementListeners))
+      if(Object.keys(elementListeners).length > 0) {
+        jQueryListeners.push(elementListeners)
+      }
+    })
+  //console.log(jQueryListeners)
+  recursiveEnumerate(jQueryListeners, 0)
+}
+
 console.log('Created by Davide Cavallini')
 console.log('Linkedin: https://www.linkedin.com/in/davidecavallini/')
 console.log('----------------------------------------------------------')
-console.log('\n')
+console.log('\n') 
 
 recursiveEnumerate(window, 0)
-searchInside(document.body.innerHTML.replace(/(\r\n|\n|\r)/gm, '').replace(/\s\s+/g, ' '), document.body, [], 0, 0)
+recursiveEnumerate(listAllEventListeners(), 0)
+searchInside(document.body.innerHTML.replace(/(\r\n|\n|\r)/gm, '').replace(/\s\s+/g, ' '), document.body, ['BODY'], 0, 0)
+recursiveEnumerate(getjQueryEventHandlers(document), 0)
+searchJqueryListeners()
 
 console.log('\n')
 console.log('----------------------------------------------------------')
